e97ea995da66dad8f31e8c699cbe555b514173ab
ichsanulalifwan <ichsanulalifwan@gmail.com>
2022-11-01 12:25:15 +0800

    implement layout and add new layout for item

---
+++ app/src/main/java/com/app/ichsanulalifwan/tasklist/TaskListFragment.kt
@@ -0,0 +1,55 @@
+package com.app.ichsanulalifwan.tasklist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.ichsanulalifwan.tasklist.databinding.FragmentTaskListBinding
+
+class TaskListFragment : Fragment() {
+
+    private var _binding: FragmentTaskListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTaskListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        setupRecyclerView()
+        showLoading(true)
+    }
+
+    private fun initView() {
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        with(binding.rvTaskList) {
+            layoutManager = LinearLayoutManager(context)
+//            adapter = rvAdapter
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) binding.progressBar.visibility = View.VISIBLE
+        else binding.progressBar.visibility = View.GONE
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
