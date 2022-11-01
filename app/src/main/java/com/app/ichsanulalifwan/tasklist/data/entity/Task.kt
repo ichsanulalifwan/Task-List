ac4649bf6e88311da31afe6e7645e585a77cfab1
ichsanulalifwan <ichsanulalifwan@gmail.com>
2022-11-01 10:06:09 +0800

    add entity

---
+++ app/src/main/java/com/app/ichsanulalifwan/tasklist/data/entity/Task.kt
@@ -0,0 +1,27 @@
+package com.app.ichsanulalifwan.tasklist.data.entity
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "task_entities")
+data class Task(
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
+    var id: Int,
+
+    @ColumnInfo(name = "text")
+    var text: String,
+
+    @ColumnInfo(name = "date")
+    var date: Date,
+
+    @ColumnInfo(name = "order")
+    var order: Int,
+
+    @ColumnInfo(name = "done")
+    var done: Boolean = false
+)
