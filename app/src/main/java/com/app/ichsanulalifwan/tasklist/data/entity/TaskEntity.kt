49c3c9c04665771dffc58e45292a1682337b3cbb
ichsanulalifwan <ichsanulalifwan@gmail.com>
2022-11-02 16:19:42 +0800

    remove unused import

--- app/src/main/java/com/app/ichsanulalifwan/tasklist/data/entity/TaskEntity.kt
+++ app/src/main/java/com/app/ichsanulalifwan/tasklist/data/entity/TaskEntity.kt
@@ -1,6 +1,5 @@
 package com.app.ichsanulalifwan.tasklist.data.entity
 
-import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
